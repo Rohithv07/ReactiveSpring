6bba12215b2b4613c06550e046b382532fef71f0
Rohithv07 <rohithv63@gmail.com>
2021-08-10 18:06:05 +0530

    Added some more test cases and one is failing for some reason

--- ReactiveProgrammingIntro/ReactiveExamples/src/test/java/com/rohith/repository/PersonRepositoryImplTest.java
+++ ReactiveProgrammingIntro/ReactiveExamples/src/test/java/com/rohith/repository/PersonRepositoryImplTest.java
@@ -1,16 +1,18 @@
 package com.rohith.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.rohith.domain.Person;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 class PersonRepositoryImplTest {
-	
+
 	PersonRepositoryImpl personRepository;
 
 	@BeforeEach
@@ -24,15 +26,25 @@ class PersonRepositoryImplTest {
 		Person person = personMono.block();
 		System.out.println(person.toString());
 	}
-	
+
 	@Test
 	void getByIdSubscribe() {
 		Mono<Person> personMono = personRepository.getById(1);
+		StepVerifier.create(personMono).expectNextCount(1).verifyComplete();
 		personMono.subscribe(person -> {
 			System.out.println(person.toString());
 		});
 	}
 	
+	@Test
+	void getByIdSubscribeNotFound() {
+		Mono<Person> personMono = personRepository.getById(9);
+		StepVerifier.create(personMono).verifyComplete();
+		personMono.subscribe(person -> {
+			System.out.println(person.toString());
+		});
+	}
+
 	@Test
 	void getByIdMapFunction() {
 		Mono<Person> personMono = personRepository.getById(1);
@@ -42,6 +54,106 @@ class PersonRepositoryImplTest {
 		}).subscribe(firstName -> {
 			System.out.println(firstName);
 		});
+	}
+
+	@Test
+	void fluxTestBlockFirst() {
+		Flux<Person> personFlux = personRepository.findAll();
+		Person person = personFlux.blockFirst();
+		System.out.println(person.toString());
+	}
+
+	@Test
+	void fluxTestSubscribe() {
+		Flux<Person> personFlux = personRepository.findAll();
+		StepVerifier.create(personFlux).expectNextCount(4).verifyComplete();
+		personFlux.subscribe(person -> {
+			System.out.println(person.toString());
+		});
+	}
+
+	@Test
+	void fluxTestListMono() {
+		Flux<Person> personFlux = personRepository.findAll();
+		Mono<List<Person>> personListMono = personFlux.collectList();
+		personListMono.subscribe(list -> {
+			list.forEach(person -> {
+				System.out.println(person.toString());
+			});
+		});
+	}
+
+	@Test
+	void testFindPersonById() {
+		Flux<Person> personFlux = personRepository.findAll();
+
+		final Integer id = 3;
+
+		Mono<Person> personMono = personFlux.filter(person -> person.getId() == id).next();
+		personMono.subscribe(person -> {
+			System.out.println(person.toString());
+		});
+
+	}
+
+	@Test
+	void testFindPersonById_NotFound() {
+		Flux<Person> personFlux = personRepository.findAll();
+
+		final Integer id = 7;
+
+		Mono<Person> personMono = personFlux.filter(person -> person.getId() == id).next();
+		personMono.subscribe(person -> {
+			System.out.println(person.toString());
+		});
+
+	}
+	
+	@Test
+	void testFindPersonById_NotFoundNext() {
+		Flux<Person> personFlux = personRepository.findAll();
+
+		final Integer id = 7;
+
+		Mono<Person> personMono = personFlux.filter(person -> person.getId() == id).next();
+		personMono.doOnError(throwable -> {
+			System.out.println("No user with id" + id);
+		}).subscribe(person -> {
+			System.out.println(person.toString());
+		});
+
+	}
+
+	@Test
+	void testFindPersonById_NotFoundWithException() {
+		Flux<Person> personFlux = personRepository.findAll();
+
+		final Integer id = 7;
+
+		Mono<Person> personMono = personFlux.filter(person -> person.getId() == id).single(); // single signals for no
+																								// such element
+		personMono.doOnError(throwable -> {
+			System.out.println("Not found, No user with id " + id);
+		}).subscribe(person -> {
+			System.out.println(person.toString());
+		});
+
+	}
+
+	@Test
+	void testFindPersonById_NotFoundWithExceptionMessage() {
+		Flux<Person> personFlux = personRepository.findAll();
+
+		final Integer id = 7;
+
+		Mono<Person> personMono = personFlux.filter(person -> person.getId() == id).single(); // single signals for no
+																								// such element
+		personMono.doOnError(throwable -> {
+			System.out.println("Not found, No user with id " + id);
+		}).onErrorReturn(new Person()).subscribe(person -> {
+			System.out.println(person.toString());
+		});
+
 	}
 
 }
